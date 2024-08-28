c8f33906d45690b59c63f059c765b11c7d62294f
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-08-28 21:16:37 +0545

    Add note creation unit test

--- backend/note_api/tests.py
+++ backend/note_api/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from .models import Note
 
-# Create your tests here.
+
+class NoteAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username="ball", password="zeldarpg")
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_note(self):
+        response = self.client.post(
+            "/api/notes/create/",
+            {"title": "Test Note", "content": "This is test note"},
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Note.objects.count(), 1)
