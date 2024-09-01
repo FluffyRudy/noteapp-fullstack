93a052a89687b15e553013d0a4d9ed8b2a9682ba
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-09-01 16:03:41 +0545

    Redirect user to home page if user is already authenticated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,11 @@
+import { Navigate } from "react-router-dom";
 import CommonForm from "../component/CommonForm";
 import { ENDPOINTS } from "../constants";
+import { useAuth } from "../context/AuthContext";
 
 export default function Login() {
+    const { authenticated } = useAuth();
+    if (authenticated) return <Navigate to={ENDPOINTS.HOME.route} />;
     return (
         <CommonForm
             route={ENDPOINTS.LOGIN.route}
