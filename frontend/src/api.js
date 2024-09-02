b9bbd3d36128c68adc3a6d238a7899902b84632d
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-09-02 10:33:21 +0545

    feat: Implement token refresh logic in Axios interceptor
    - Added logic to automatically refresh access tokens when they expire.
    - Prevents unauthorized access errors for users who remain logged in for extended periods.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ACCESS_TOKEN } from "./constants";
+import { ACCESS_TOKEN, ENDPOINTS, REFRESH_TOKEN } from "./constants";
 
 const API = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -15,6 +15,44 @@ API.interceptors.request.use(
     },
     (error) => {
         Promise.reject(error);
+    }
+);
+
+API.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+
+            try {
+                const response = await API.post(
+                    ENDPOINTS.NEW_REFRESH_TOKEN.route,
+                    { refresh: refreshToken }
+                );
+
+                if (response.status === 200) {
+                    const { access, refresh } = response.data;
+                    localStorage.setItem(ACCESS_TOKEN, access);
+                    localStorage.setItem(REFRESH_TOKEN, refresh);
+
+                    API.defaults.headers.common[
+                        "Authorization"
+                    ] = `Bearer ${access}`;
+                    originalRequest.headers[
+                        "Authorization"
+                    ] = `Bearer ${access}`;
+
+                    return API(originalRequest);
+                }
+            } catch (err) {
+                console.error("Token refresh failed:", err);
+            }
+        }
+
+        return Promise.reject(error);
     }
 );
 
