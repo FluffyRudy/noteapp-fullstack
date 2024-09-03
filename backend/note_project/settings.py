7ea0afa82d75f723ab33ec4585f3995f3d4045a0
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-09-03 06:54:04 +0545

    Specify cors allowed origins

--- backend/note_project/settings.py
+++ backend/note_project/settings.py
@@ -138,5 +138,7 @@ STATIC_URL = "static/"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.vercel\.app$",
+]
 CORS_ALLOW_CREDENTIALS = True
